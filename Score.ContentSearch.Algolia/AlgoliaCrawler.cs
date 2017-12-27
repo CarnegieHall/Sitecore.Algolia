d6c4ec32b51dda739553a9939602a2acae0bb36e
Michael Peppler <mpeppler@carnegiehall.org>
2017-12-27 14:28:08 -0500

    add hide from search results field to crawler
    Useful if you only want to exclude some items from search.
    
    Also made sure to include fixes if the field is a bool

--- Score.ContentSearch.Algolia/AlgoliaCrawler.cs
+++ Score.ContentSearch.Algolia/AlgoliaCrawler.cs
@@ -3,9 +3,10 @@ using Sitecore.Data.Items;
 
 namespace Score.ContentSearch.Algolia
 {
-    public class AlgoliaCrawler: SitecoreItemCrawler
+    public class AlgoliaCrawler : SitecoreItemCrawler
     {
         public string ShowInSearchResultsFieldName { get; set; }
+        public string HideFromSearchResultsFieldName { get; set; }
 
         protected override bool IsExcludedFromIndex(SitecoreIndexableItem indexable, bool checkLocation = false)
         {
@@ -22,6 +23,29 @@ namespace Score.ContentSearch.Algolia
                 if (showInSearchResultsField != null)
                 {
                     result = string.IsNullOrWhiteSpace(showInSearchResultsField.Value);
+                }
+            }
+
+            if (!string.IsNullOrWhiteSpace(HideFromSearchResultsFieldName))
+            {
+                var hideFromSearchResultsField = obj.Fields[HideFromSearchResultsFieldName];
+                if (hideFromSearchResultsField != null)
+                {
+                    if (bool.TryParse(hideFromSearchResultsField.Value, out result))
+                    {
+                        // If the value is a bool return that value
+                        return result;
+
+                    }
+                    else if (hideFromSearchResultsField.Value == "0")
+                    {
+                        // If the value is a zero, it's probably a bool and return false
+                        return false;
+                    }
+                    else
+                    {
+                        result = !string.IsNullOrWhiteSpace(hideFromSearchResultsField.Value);
+                    }
                 }
             }
 
